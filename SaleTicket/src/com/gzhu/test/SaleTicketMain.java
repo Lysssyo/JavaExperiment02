e4b60d05cfa9e91b62b523d974afe36a372a5350
Lysssyo <1986355071@qq.com>
2023-12-05 15:55:26 +0800

    Revert "添加了一个名为Counter5的新的线程"
    This reverts commit 8839c2af602fc3a8e4a2ef2e04c07afe6f76b4e8.

--- SaleTicket/src/com/gzhu/test/SaleTicketMain.java
+++ SaleTicket/src/com/gzhu/test/SaleTicketMain.java
@@ -6,12 +6,10 @@ public class SaleTicketMain {
         Station Counter2=new Station("Counter2");
         Station Counter3=new Station("Counter3");
         Station Counter4=new Station("Counter4");
-        Station Counter5=new Station("Counter4");
         Counter1.start();
         Counter2.start();
         Counter3.start();
         Counter4.start();
-        Counter5.start();
 
     }
 
