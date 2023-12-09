8e5fc9f9a1249b8b939a84818f69fd7b7f0155c3
Lysssyo <1986355071@qq.com>
2023-12-09 16:52:48 +0800

    添加了详尽的注释

--- SaleTicket/src/com/gzhu/test/SaleTicketMain.java
+++ SaleTicket/src/com/gzhu/test/SaleTicketMain.java
@@ -2,9 +2,11 @@ package com.gzhu.test;
 
 public class SaleTicketMain {
     public static void main(String[] args) {
+        //设置三个售票窗口
         Station Counter1=new Station("Counter1");
         Station Counter2=new Station("Counter2");
         Station Counter3=new Station("Counter3");
+        //三个窗口同时卖票
         Counter1.start();
         Counter2.start();
         Counter3.start();
--- SaleTicket/src/com/gzhu/test/Station.java
+++ SaleTicket/src/com/gzhu/test/Station.java
@@ -1,33 +1,42 @@
 package com.gzhu.test;
 
+// 定义一个站点类，继承自Thread类
 public class Station extends Thread {
-    //这个类的所有对象共享ticket这一数据
-    static  int ticket=0;
-    static boolean ticketsSoldOut = false;
-    static Object obj=new Object();
 
+    // 所有对象共享的卖票数据
+    static int ticket = 0;  // 卖出的票数，初始化为0
+    static boolean ticketsSoldOut = false;  // 是否卖光了的标志，初始化为false
+    static Object obj = new Object();  // 对象锁，用于同步
+
+    //定义一个带参的构造函数，用于新建对象的时间可以顺便把名字给赋值
     public Station(String name) {
-        super(name);
+        super(name);  // 调用Thread类的构造方法，设置线程的名称
     }
 
+    // 线程执行的主体方法
     @Override
     public void run() {
+        // 循环执行卖票的操作
         while (true) {
-            synchronized (obj){
-                if (ticket<100){
-                    ticket++;
+            // 使用 synchronized 关键字保证线程安全，锁定 obj 对象
+            synchronized (obj) {
+                // 如果卖的票数小于20，继续卖票
+                if (ticket < 20) {
+                    ticket++;  // 增加卖出的票数
+                    // 输出卖票信息
                     System.out.println(Thread.currentThread().getName() + "卖出了第" + ticket + "张票");
-                }
-                else{
+                } else {
+                    // 如果票卖完了且还没有标记为卖光
                     if (!ticketsSoldOut) {
                         System.out.println("票卖完了！");
-                        ticketsSoldOut = true;
+                        ticketsSoldOut = true;  // 标记为卖光
                     }
+                    // 退出循环，结束线程执行
                     break;
                 }
             }
-            Thread.yield();//随机一点
+            Thread.yield();  // 重新分配CPU，释放一些资源给其他线程
         }
-
     }
 }
+
